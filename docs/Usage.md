08ef7948911ec8e8bb4911ff78231dfc1c45dc61
MysticTortoise <shadowknight7788@gmail.com>
2026-01-12 14:14:55 -0700

    Writing

--- docs/Usage.md
+++ docs/Usage.md
@@ -1,6 +1,2 @@
 # Usage
-Include "ReStructer.hpp" whenever you want to call ReadValue().
-
-"ReStructerLast.hpp" should be included after most headers where possible, 
-as some issues with template ordering make this necessary. 
-It seems to work fine for now, but you never know later down the line. ;)
+Include `"ReStructer/Reading.hpp"` whenever you want to call ReadValue().
---
+++ library/include/ReStructer/Writing.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <fstream>
+#include <cstdint>
+#include <array>
+#include <vector>
+#include <iostream>
+
+namespace ReStructer
+{
+#include "ReStructer/private/Concepts.hpp"
+
+    // Basic Template Inits
+    template<typename T> void WriteValue(const T& v, std::ofstream& stream) = delete;
+    template<typename T> void WriteBigEndian(const T& v, std::ofstream& stream) = delete;
+    template<typename T> void WriteLittleEndian(const T& v, std::ofstream& stream) = delete;
+
+    // Basic Reads
+#define BasicEntry(Type) \
+    template<> void WriteValue<Type>(const Type&, std::ofstream&); \
+    template<> void WriteBigEndian<Type>(const Type&, std::ofstream&); \
+    template<> void WriteLittleEndian<Type>(const Type&, std::ofstream&);
+    // ReSharper disable once CppUnusedIncludeDirective
+#include "ReStructer/private/BasicEntries.hpp"
+#undef BasicEntry
+
+    template<> void WriteValue(const std::string& v, std::ofstream& stream);
+
+    // ----------------------------------------------------------------------------------------------------------------
+    // START OF SUBTEMPLATE STUFF
+    // ----------------------------------------------------------------------------------------------------------------
+
+    // Arrays
+#define ArrayFunc(Func) \
+    template<IsStdArray T> void Func(const T& v, std::ofstream& stream) \
+    { \
+        for (size_t i = 0; i < v.size(); ++i) \
+        { \
+            Func(v[i], stream); \
+        } \
+    }
+    ArrayFunc(WriteValue)
+    ArrayFunc(WriteBigEndian)
+    ArrayFunc(WriteLittleEndian)
+#undef ArrayFunc
+
+    // Vectors
+#define VectorFunc(Func) \
+    template<IsStdVector T> void Func(const T& v, std::ofstream& stream) \
+    { \
+        WriteLittleEndian(static_cast<uint32_t>(v.size()), stream); \
+        for (size_t i = 0; i < v.size(); i++) \
+        { \
+            Func(v[i], stream); \
+        } \
+    }
+
+    VectorFunc(WriteValue)
+    VectorFunc(WriteBigEndian)
+    VectorFunc(WriteLittleEndian)
+#undef VectorFunc
+    
+}
